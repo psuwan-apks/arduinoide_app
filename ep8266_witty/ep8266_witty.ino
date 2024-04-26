921a9f05a17ed16ee1b06d8bda2772611abb7046
Pattanapong Suwan <psuwan.apks@gmail.com>
2024-04-26 16:06:25 +0700

    blinking esp8266 witty cloud on board LED
    with basic WiFi connecting code

--- ep8266_witty/ep8266_witty.ino
+++ ep8266_witty/ep8266_witty.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
 
+const char* ssid = "YourSSID";
+const char* password = "YourPassword";
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED on
+  delay(1000);                      // Wait for 1 second
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED off
+  delay(1000);                      // Wait for 1 second
 }
